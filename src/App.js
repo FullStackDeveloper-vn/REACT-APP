417ced10d7270dafaf1f01f6c850a0c9adfacc8a
new121711 <alohikk@gmail.com>
2023-10-03 04:15:40 +0700

    Ramp Challenge

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
-import { useState } from "react";
-
-function Lists({ arr }) {
-
-  return (
-    <>
-      <ul>
-        {arr.map((a) => (
-          <li>{a}</li>
-        ))}
-      </ul>
-    </>
-  )
-}
+import { useState, useEffect, ref } from "react";
 
 export default function App() {
-  const [arr, setArr] = useState([''])
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  const [arr, setArr] = useState([]);
+
+  useEffect(() => {
+    fetch("https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/637261")
+      .then(res => res.text())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.split(''));
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
 
   function renderLists() {
-    if (arr.length < 5) {
+    if (arr.length < items.length) {
+      let l = arr.length
       setArr([
-        ...arr, ['x']
+        ...arr, items[l]
       ])
       clearInterval(intervalID)
     }
   }
 
-  const intervalID = setInterval(renderLists, 1000);
+  let intervalID = setInterval(renderLists, 500)
 
-  console.log(arr.length);
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <Lists array={arr} />
+    );
+  }
+}
+
+function Lists({ array }) {
   return (
     <>
-      <Lists arr={arr} />
+      <ul>
+        {array.map((element, index) => (
+          <li key={index}>{element}</li>
+        ))}
+      </ul>
     </>
   )
-};
+}
