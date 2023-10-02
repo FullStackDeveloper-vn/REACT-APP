dfa62a30ee34c22e94b2c573e3d81fafdbba75bc
new121711 <alohikk@gmail.com>
2023-10-03 01:51:10 +0700

    setInterVal- renderLists

--- src/App.js
+++ src/App.js
@@ -1,68 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+
+function Lists({ arr }) {
+
+  return (
+    <>
+      <ul>
+        {arr.map((a) => (
+          <li>{a}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
 
 export default function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-  const [count, setCount] = useState(0);
-  const [oldCount, setOldCount] = useState('');
+  const [arr, setArr] = useState([''])
 
-
-
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
-  useEffect(() => {
-    fetch("https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/637261")
-      .then(res => res.text())
-      .then(
-        (result) => {
-
-          setIsLoaded(true);
-          setItems(result.split(''));
-
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
-
-  useEffect(() => {
-    console.log('✅ Creating an interval');
-    const id = setInterval(() => {
-      console.log('⏰ Interval tick');
-      setCount(count + 1);
-    }, 1000);
-    return () => {
-      console.log('❌ Clearing an interval');
-      clearInterval(id);
-    };
-  }, [count]);
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <>
-        {count} <br />
-
-
-        <ul>
-          {items.map((item, index) => (
-            <li>
-              {item}
-            </li>
-          ))}
-        </ul>
-      </>
-
-    );
+  function renderLists() {
+    if (arr.length < 5) {
+      setArr([
+        ...arr, ['x']
+      ])
+      clearInterval(intervalID)
+    }
   }
-}
+
+  const intervalID = setInterval(renderLists, 1000);
+
+  console.log(arr.length);
+  return (
+    <>
+      <Lists arr={arr} />
+    </>
+  )
+};
