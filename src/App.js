ed21969c1dac1945dcd91bb8c6c150f941bf3d3e
new121711 <alohikk@gmail.com>
2023-10-08 17:12:20 +0700

    Challenge Context ???

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-
-import AddTask from "./AddTask";
-import TaskList from "./TaskList";
-import { TasksProvider } from "./TasksContext";
+import { useState } from "react";
+import { places } from "./data";
+import { getImageUrl } from "./utils";
 
 export default function App() {
-  return (
-    <TasksProvider>
-      <h1>Day off in Kyoto</h1>
-      <AddTask />
-      <TaskList />
-    </TasksProvider>
-  )
+  const [isLarge, setIsLarge] = useState(false)
+  const imageSize = isLarge ? 150 : 100
 
+  return (
+    <>
+      <label>
+        <input type="checkbox" checked={isLarge} onChange={e => {
+          setIsLarge(e.target.checked)
+        }} />
+        Use large images
+      </label>
+      <hr />
+      <List imageSize={imageSize} />
+    </>
+  )
 };
 
+function List({ imageSize }) {
+  const listItems = places.map(place => <li key={place.id} >
+    <Place place={place} imageSize={imageSize} />
+  </li>)
+  return <ul>{listItems} </ul>
+}
+
+function Place({ place, imageSize }) {
+  return (
+    <>
+      <PlaceImage place={place} imageSize={imageSize} />
+      <p>
+        <b>
+          {place.name}
+        </b>
+        {': ' + place.description}
+      </p>
+    </>
+  )
+}
+
+function PlaceImage({ place, imageSize }) {
+  return (
+    <img src={getImageUrl(place)} alt={place.name} width={imageSize} height={imageSize} />
+  )
+}
