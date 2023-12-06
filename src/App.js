0617a35e5e14fba8d7ed002536807bb634d5322e
alohikk <alohikk@gmail.com>
2023-12-07 06:28:54 +0700

    Restfull Api use SWR

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import axios from "axios"
-import { useState,useEffect } from "react"
+import useSWR from "swr"
+
+const fetcher = (...args) => fetch(...args)
+  .then((res) => res.json())
 
 export default function App() {
-  const [items,setItems] = useState([])
-  const [error,setError] = useState(null)
+  const { data, error } = useSWR(
+    'https://jsonplaceholder.typicode.com/users',
+    fetcher
+  )
 
-  useEffect(() => {
-    axios('https://jsonplaceholder.typicode.com/users')
-      .then((response) => {
-  console.log(response.data)
-
-        setItems(response.data)
-        setError(null)
-      })
-      .catch(setError)
-  },[])
-
-  if(error) return <p>An error occurred</p>
+  if (error) return <p>An error occurred</p>;
+  if (!data) return <p>Loading</p>;
+  console.log(data)
   return (
     <ul>
-    {items.map(item => (
-      <li key={item.id}>
-        {item.name} <br></br> {item.email}
-      </li>
-    ))}
-  </ul>
+      {data.map(item => (
+        <li key={item.id}>
+          {item.id} <br></br>
+          {item.name} <br></br> {item.email} <hr></hr>
+        </li>
+      ))}
+    </ul>
   )
 }
 
