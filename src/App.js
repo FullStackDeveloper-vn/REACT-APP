bf39a5e1ed6bf608045428a3197caf4003544b9e
alohikk <alohikk@gmail.com>
2023-12-19 23:09:11 +0700

    Snow Fall

--- src/App.js
+++ src/App.js
@@ -1,71 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import ChartComponent from './components/ChartComponent';
+import React, { useEffect, useRef } from 'react';
+
+function Snowfall({ count }) {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const snowflakes = [];
+
+    const createSnowflake = () => ({
+      x: Math.random() * canvas.width,
+      y: 0,
+      radius: Math.random() * 2 + 1,
+      speed: Math.random() * 3 + 1,
+      opacity: Math.random() * 0.5 + 0.5,
+    });
+
+
+
+    const drawSnowflake = (snowflake) => {
+      ctx.beginPath();
+      ctx.arc(snowflake.x, snowflake.y, snowflake.radius, 0, Math.PI * 2, false);
+      ctx.fillStyle = `rgba(255, 255, 255, ${snowflake.opacity})`;
+      ctx.fill();
+    };
+
+    const updateSnowflakes = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      snowflakes.forEach((snowflake, index) => {
+        snowflake.y += snowflake.speed;
+
+        if (snowflake.y > canvas.height) {
+          snowflakes.splice(index, 1);
+        }
+
+        drawSnowflake(snowflake);
+      });
+
+      while (snowflakes.length < count) {
+        snowflakes.push(createSnowflake());
+      }
+    };
+
+    const animate = () => {
+      updateSnowflakes();
+      requestAnimationFrame(animate);
+    };
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    animate();
+
+    // Clean up event listeners when component unmounts
+    return () => cancelAnimationFrame(animate);
+  }, [count]);
+
+  return <canvas ref={canvasRef} />;
+};
+
 
 export default function App() {
   return (
-    <>
-      <div className="header">
-        <h1>Admin Panel</h1>
-      </div>
-
-      <div class="sidebar">
-        <ul>
-          <li className='folder'><a href="#">Dashboard</a></li>
-          <li className='file'><a href="#">Users</a></li>
-          <li className='file'><a href="#">Settings</a></li>
-          <li className='folder'><a href="#">Logout</a></li>
-        </ul>
-      </div>
-
-
-      <div className="container">
-        <div className="content column">
-          <h2>Welcome to the Admin Panel</h2>
-
-          <p>This is the main content area of the admin panel.</p>
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Country</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>John Doe</td>
-                <td>USA</td>
-                <td>25</td>
-              </tr>
-              <tr>
-                <td>Jane Doe</td>
-                <td>USA</td>
-                <td>30</td>
-              </tr>
-              <tr>
-                <td>John Smith</td>
-                <td>UK</td>
-                <td>35</td>
-              </tr>
-              <tr>
-                <td>Jane Smith</td>
-                <td>UK</td>
-                <td>40</td>
-              </tr>
-            </tbody>
-          </table>
-
-
-        </div>
-        <div className='column'>
-          <ChartComponent />
-        </div>
-      </div>
-
-      <div class="footer">
-        <p>Admin Panel &copy; 2021</p>
-      </div>
-
-    </>
-  )
-}
+    <div className='bg'>
+      {/* You can adjust the count prop to control the number of snowflakes */}
+      <Snowfall count={100} />
+    </div>
+  );
+};
