4221fcf27687221ceec3016ec8a452ecd0d89160
alohikk <alohikk@gmail.com>
2023-12-23 23:43:12 +0700

    Bubbles

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ REACT CODE
 
 ![Alt text](Untitled.png)
 
+![Alt text](Untitled-1.png)
 Change Github
--- src/App.js
+++ src/App.js
@@ -1,71 +1,85 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect } from "react";
 
-function Snowfall({ count }) {
-  const canvasRef = useRef(null);
+const App = () => {
+  const [bubbles, setBubbles] = useState([]);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    const snowflakes = [];
+  // Function to generate random number between min and max
+  const getRandomNumber = (min, max) => {
+    return Math.random() * (max - min) + min;
+  };
 
-    const createSnowflake = () => ({
-      x: Math.random() * canvas.width,
-      y: 0,
-      radius: Math.random() * 2 + 1,
-      speed: Math.random() * 3 + 1,
-      opacity: Math.random() * 0.5 + 0.5,
+  // Function to generate a new bubble
+  const createBubble = () => {
+    const bubble = {
+      id: Date.now(),
+      x: getRandomNumber(0, window.innerWidth), // random X position within the window width
+      y: -50, // starting Y position above the screen
+      size: getRandomNumber(50, 90), // random bubble size
+      speed: 0.5 // random falling speed
+    };
+
+    setBubbles((prevState) => [...prevState, bubble]);
+  };
+
+  // Function to update bubble position on each animation frame
+  const updateBubblePosition = () => {
+    setBubbles((prevState) => {
+      return prevState.map((bubble) => ({
+        ...bubble,
+        y: bubble.y + bubble.speed // update Y position by adding the falling speed
+      }));
     });
+  };
 
+  // Function to remove bubbles that go below the screen
+  const removeBubbles = () => {
+    setBubbles((prevState) => {
+      return prevState.filter((bubble) => bubble.y <= window.innerHeight);
+    });
+  };
 
+  // Effect hook to create bubbles on component mount
+  useEffect(() => {
+    const interval = setInterval(createBubble, 500); // create a new bubble every 0.5 seconds
 
-    const drawSnowflake = (snowflake) => {
-      ctx.beginPath();
-      ctx.arc(snowflake.x, snowflake.y, snowflake.radius, 0, Math.PI * 2, false);
-      ctx.fillStyle = `rgba(255, 255, 255, ${snowflake.opacity})`;
-      ctx.fill();
+    // Clean up function to remove interval on component unmount
+    return () => {
+      clearInterval(interval);
     };
+  }, []);
 
-    const updateSnowflakes = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // Effect hook to update bubble positions and remove bubbles
+  useEffect(() => {
+    const animationFrame = requestAnimationFrame(updateBubblePosition);
 
-      snowflakes.forEach((snowflake, index) => {
-        snowflake.y += snowflake.speed;
-
-        if (snowflake.y > canvas.height) {
-          snowflakes.splice(index, 1);
-        }
-
-        drawSnowflake(snowflake);
-      });
-
-      while (snowflakes.length < count) {
-        snowflakes.push(createSnowflake());
-      }
+    // Clean up function to cancel animation frame on component unmount
+    return () => {
+      cancelAnimationFrame(animationFrame);
     };
+  });
 
-    const animate = () => {
-      updateSnowflakes();
-      requestAnimationFrame(animate);
-    };
+  // Effect hook to remove bubbles that go below the screen
+  useEffect(() => {
+    removeBubbles();
+  }, [bubbles]);
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    animate();
-
-    // Clean up event listeners when component unmounts
-    return () => cancelAnimationFrame(animate);
-  }, [count]);
-
-  return <canvas ref={canvasRef} />;
-};
-
-
-export default function App() {
   return (
-    <div className='bg'>
-      {/* You can adjust the count prop to control the number of snowflakes */}
-      <Snowfall count={100} />
+    <div className="bubble-container">
+      {bubbles.map((bubble) => (
+        <div
+          key={bubble.id}
+          className="bubble"
+          style={{
+            left: bubble.x,
+            top: bubble.y,
+            width: bubble.size,
+            height: bubble.size
+          }}
+        />
+      ))}
+      <div className="screen" />
     </div>
   );
 };
+
+export default App;
